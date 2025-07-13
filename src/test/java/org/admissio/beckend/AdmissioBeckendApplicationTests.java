4f6942cd33e5ff20ed6029afc865abf0a4cdd3e3
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2025-07-13 21:11:26 +0300

    feat: maven init

---
+++ src/test/java/org/admissio/beckend/AdmissioBeckendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.admissio.beckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissioBeckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
