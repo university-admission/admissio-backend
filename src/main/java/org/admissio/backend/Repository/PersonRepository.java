7f99b8524ff4febcaae521bf9d1a502db42f095e
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2025-07-18 10:50:34 +0300

    fix: remove temp classes

--- src/main/java/org/admissio/backend/Repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.admissio.backend.Repository;
-
-import org.admissio.backend.Entity.PersonEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<PersonEntity, Integer> {
-}
