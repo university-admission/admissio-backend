cfd26678c03cf67945e2cd88ef9bbe51caec198a
hrustikG <113693682+hrustikG@users.noreply.github.com>
2025-07-27 12:56:08 +0300

    fix: change university region API

--- src/main/java/org/admissio/backend/controller/RegionController.java
+++ src/main/java/org/admissio/backend/controller/RegionController.java
@@ -1,7 +1,10 @@
 package org.admissio.backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.admissio.backend.service.UniversityService;
+import org.admissio.backend.entity.UniversityRegion;
+import org.admissio.backend.service.UniversityRegionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,10 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RegionController {
 
-    private final UniversityService universityService;
+    private final UniversityRegionService universityRegionService;
 
     @GetMapping
-    public List<String> getAllRegions() {
-        return universityService.getAllCities();
+    public ResponseEntity<List<UniversityRegion>> getAllRegions() {
+        return new ResponseEntity<>(universityRegionService.findAll(), HttpStatus.OK);
     }
 }
