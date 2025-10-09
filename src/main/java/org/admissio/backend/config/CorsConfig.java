70d3bd5d97898d6b4422705d5607f4b355310074
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2025-10-09 19:07:16 +0300

    fix: move CorsConfig.java to config folder

--- src/main/java/org/admissio/backend/config/CorsConfig.java
+++ src/main/java/org/admissio/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.admissio.backend;
+package org.admissio.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
